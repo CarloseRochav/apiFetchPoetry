937890d86401abeb0890de0afc0fd792b876a735
carloserochav <g14ada02@hackademy.mx>
2023-02-15 23:21:01 -0800

    Capturado el autor por input

--- index.js
+++ index.js
@@ -2,29 +2,40 @@
 
         //Elementos
         const formData = document.getElementById("formData");       
-        const titlePoetry=document.getElementById("inPoema");
+        //const titlePoetry=document.getElementById("inPoema");
+        const inAutor = document.getElementById("inAutor");
 
-
-        formData.addEventListener("submit",(e)=>{
-          e.preventDefault();
-          console.log("Poema : "+titlePoetry.value)
-
-        })
 
         //Endpoints
         const urlPoema="https://poetrydb.org/title/Ozymandias/lines.json"
         const urlAutores="https://poetrydb.org/author"
-        const urlAuthorTitles="https://poetrydb.org/author/Wilde" //Obras del artista
+        const urlAuthorTitles="https://poetrydb.org/author/" //Obras del artista
         const urlAuthorPoetry="https://poetrydb.org/title/Charmides/lines.json" //Titulo en especifico
 
-                fetch(urlAuthorPoetry)
+
+        formData.addEventListener("submit",(e)=>{
+          e.preventDefault();
+          //console.log("Poema : "+titlePoetry.value)
+          console.log("Autor : "+inAutor.value)
+
+          //Fetch
+          fetch(urlAuthorTitles+inAutor.value)//Concateno el autor capturado por el formulario
                         .then((resp) => resp.json())
                         .then(function (data) {   
-                            const poetry = data[0].lines                        
+                            const obras = data
                             //console.log("Poema : "+titlePoetry.value)
-                            console.log(poetry)//Muestra solo las lineas del poema
+                            console.log(obras)//Muestra solo las lineas del poema
                         })
 
                         .catch(function (error) {
                             console.log(error);
                         });
+
+        })
+
+        
+
+
+        //Elementos con las obras
+
+                
