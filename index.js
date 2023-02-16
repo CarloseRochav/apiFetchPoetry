644856746f6dc7e2922df1b2c8636af8579a7072
carloserochav <g14ada02@hackademy.mx>
2023-02-15 23:07:08 -0800

    Jalar ifnformacion del form

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 "use strict"
 
-        //Inputs
+        //Elementos
+        const formData = document.getElementById("formData");       
         const titlePoetry=document.getElementById("inPoema");
+
+
+        formData.addEventListener("submit",(e)=>{
+          e.preventDefault();
+          console.log("Poema : "+titlePoetry.value)
+
+        })
 
         //Endpoints
         const urlPoema="https://poetrydb.org/title/Ozymandias/lines.json"
@@ -13,7 +21,7 @@
                         .then((resp) => resp.json())
                         .then(function (data) {   
                             const poetry = data[0].lines                        
-                            console.log("Poema : "+titlePoetry.value)
+                            //console.log("Poema : "+titlePoetry.value)
                             console.log(poetry)//Muestra solo las lineas del poema
                         })
 
