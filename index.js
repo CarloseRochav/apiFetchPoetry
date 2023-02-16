ce53d111d889d7e826c1e0e85d94d816c8aaed43
carloserochav <g14ada02@hackademy.mx>
2023-02-15 22:13:13 -0800

    Input

--- index.js
+++ index.js
@@ -1,14 +1,22 @@
+"use strict"
 
-const newUrl="https://poetrydb.org/title/Ozymandias/lines.json"
+        //Inputs
+        const titlePoetry=document.getElementById("inPoema");
 
+        //Endpoints
+        const urlPoema="https://poetrydb.org/title/Ozymandias/lines.json"
+        const urlAutores="https://poetrydb.org/author"
+        const urlAuthorTitles="https://poetrydb.org/author/Wilde" //Obras del artista
+        const urlAuthorPoetry="https://poetrydb.org/title/Charmides/lines.json" //Titulo en especifico
 
-  fetch(newUrl)
-.then((resp) => resp.json())
-.then(function(data) {
-  let poema = data.results;
-    console.log(poema)    
-  })
+                fetch(urlAuthorPoetry)
+                        .then((resp) => resp.json())
+                        .then(function (data) {   
+                            const poetry = data[0].lines                        
+                            console.log("Poema : "+titlePoetry)
+                            console.log(poetry)//Muestra solo las lineas del poema
+                        })
 
-.catch(function(error) {
-  console.log(error);
-});
+                        .catch(function (error) {
+                            console.log(error);
+                        });
